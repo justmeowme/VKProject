cdf54dfdf59fc952da10c550014b0d7d607372c0
wis4nya <wishnya23@gmail.com>
2021-10-24 14:31:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkprojectfinallast6/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.vkprojectfinallast6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiCity;
+import com.vk.sdk.api.model.VKApiUser;
+import com.vk.sdk.api.model.VKApiUserFull;
+import com.vk.sdk.api.model.VKList;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] scope = new String[] {VKScope.FRIENDS};
+    TextView mText;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VKSdk.login(this, scope);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                //OK
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+                Toast.makeText(MainActivity.this, "doesn't work", Toast.LENGTH_SHORT).show();
+                //NOT OK
+            }
+        })) super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+}
